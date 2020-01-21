4d24888e94dd2f27a6533487a627787f96e40217
XuZonghao <xuzonghao88@qq.com>
2020-01-21 17:48:37 +0800

    first commit

---
+++ src/main/java/cn/com/xu/zonghao/demo/Greeting.java
@@ -0,0 +1,9 @@
+package cn.com.xu.zonghao.demo;
+
+/**
+ * @Author: xzh
+ * @Date: 2020/1/21 17:30
+ * @Version 1.0
+ */
+public class Greeting {
+}
---
+++ src/test/java/cn/com/xu/zonghao/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xu.zonghao.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
